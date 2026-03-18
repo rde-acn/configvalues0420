90e87b2442eb39be6e24a8995947032c305b03cd
rde-acn <rde.acn.backpack@gmail.com>
2026-03-18 11:42:07 +0530

    Copy ConfigValues/AuthenticatorService.cs from source repository

---
+++ ConfigValues/AuthenticatorService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Options;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ConfigValues
+{
+    public class AuthenticatorService : IAuthenticatorService
+    {
+        AuthServiceConfiguration configuration;
+
+        IServiceProvider _serviceProvider;
+        public AuthenticatorService( IServiceProvider serviceProvider) 
+        {
+            //configuration = optionsSnapshot.Value;
+            _serviceProvider = serviceProvider;
+        }
+        public string Authenticate()
+        {
+            using (var scope = _serviceProvider.CreateScope())
+            {
+                var scopedProvider = scope.ServiceProvider;
+                var test = scopedProvider.GetRequiredService<IOptionsSnapshot<AuthServiceConfiguration>>();
+
+                if (test.Value.EnableLogin == true)
+                {
+                    return "Authenticated Successfuly !";
+                }
+                else
+                {
+                    return "Login Not Enabled !";
+                }
+
+                //...
+            }
+           
+
+        }
+    }
+}
