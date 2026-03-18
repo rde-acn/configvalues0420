497b2951e45aded3ecc65e1ec73b1283a080b315
rde-acn <rde.acn.backpack@gmail.com>
2026-03-18 11:42:17 +0530

    Copy ConfigValues/LoggingService.cs from source repository

---
+++ ConfigValues/LoggingService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace ConfigValues
+{
+    public class LoggingService : ILoggingService
+    {
+        LoggingConfiguration Configuration;
+
+        public LoggingService(IOptionsMonitor<LoggingConfiguration> optionsMonitor)
+        {
+            Configuration = optionsMonitor.CurrentValue;
+
+            optionsMonitor.OnChange(op =>
+            {
+                Configuration = op;
+            });
+        }
+        public string Log(string message)
+        {
+            if(Configuration.EnableLog == true)
+            {
+                return "Log Pushed";
+
+            } else
+            {
+                return "Log Not Enabled";
+            }
+        }
+    }
+}
