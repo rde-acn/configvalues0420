4d47bced30cff1020137ffb9a1d31343fddf0b96
rde-acn <rde.acn.backpack@gmail.com>
2026-03-18 11:42:11 +0530

    Copy ConfigValues/FeaturesConfiguration.cs from source repository

---
+++ ConfigValues/FeaturesConfiguration.cs
@@ -0,0 +1,34 @@
+﻿namespace ConfigValues
+{
+    public class FeaturesConfiguration
+    {
+        public FeaturesConfiguration() { }
+
+        public HomePage HomePage { get; set; }
+
+        public string LoggingEnabled { get; set; }
+
+    }
+
+
+    public class ChatServiceConfiguration
+    {
+        public ChatServiceConfiguration() { }
+
+        public bool FeatureEnabled { get; set; }
+    }
+
+    public class AuthServiceConfiguration
+    {
+        public AuthServiceConfiguration() { }
+
+        public bool EnableLogin { get; set; }
+    }
+
+    public class LoggingConfiguration
+    {
+        public LoggingConfiguration() { }
+
+        public bool EnableLog { get; set; }
+    }
+}
