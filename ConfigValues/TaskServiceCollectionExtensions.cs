91ce1781d47d725389681c2c18abc41e4a564da0
rde-acn <rde.acn.backpack@gmail.com>
2026-03-18 11:42:23 +0530

    Copy ConfigValues/TaskServiceCollectionExtensions.cs from source repository

---
+++ ConfigValues/TaskServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace ConfigValues
+{
+    public static class TaskServiceCollectionExtensions
+    {
+        public static IServiceCollection AddOtherServices(this IServiceCollection services,IConfiguration configuration)
+        {
+             var bEnabled = configuration.GetValue<bool>("FeaturesAvailable:HomePage:ShowLoginPage");
+            if(bEnabled)
+            {
+                services.AddSingleton<IChatService, ChatService>();
+                services.AddSingleton<IAuthenticatorService, AuthenticatorService>();
+            }
+            return services;
+        }
+    }
+}
