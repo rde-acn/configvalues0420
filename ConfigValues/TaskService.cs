6a2ea221f3b12f46868d702f34fdec9136403044
rde-acn <rde.acn.backpack@gmail.com>
2026-03-18 11:42:21 +0530

    Copy ConfigValues/TaskService.cs from source repository

---
+++ ConfigValues/TaskService.cs
@@ -0,0 +1,49 @@
+﻿namespace ConfigValues
+{
+    public class TaskService : ITaskService
+    {
+        private readonly IConfiguration _configuration;
+
+        public FeaturesAvailable fAvailable { get; set; }
+        public TaskService(IConfiguration configuration)
+        { 
+            _configuration = configuration;
+            InstantiateFeaturesAvailable();
+        }
+
+        private void InstantiateFeaturesAvailable()
+        {
+            FeaturesAvailable featuresAvailable = new FeaturesAvailable();
+            _configuration.Bind("FeaturesAvailable", featuresAvailable);
+            fAvailable = featuresAvailable;
+        }
+
+        public string? GetTask()
+        {
+            // return _configuration?.GetValue<string>("TaskDetails:TodaysTask");
+
+            return fAvailable.HomePage.ShowLoginPage.ToString();
+        }
+
+        public void SetTask(string Task)
+        {
+            Console.WriteLine("Task Assigned");
+
+        }
+
+        public bool ShowLoginPage()
+        {
+           //var show = _configuration.GetValue<bool>("FeaturesAvailable:HomePage:ShowLoginPage");
+           // return show;
+
+            var configurationValue = _configuration.GetSection("FeaturesAvailable:HomePage");
+            var show = configurationValue.GetValue<bool>("ShowLoginPage");
+            return show;
+        }
+
+        public void TaskBoard()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
