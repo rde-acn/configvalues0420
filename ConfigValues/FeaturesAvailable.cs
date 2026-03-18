e04daedc633de775c1798f7adaa8653fec6c4e45
rde-acn <rde.acn.backpack@gmail.com>
2026-03-18 11:42:10 +0530

    Copy ConfigValues/FeaturesAvailable.cs from source repository

---
+++ ConfigValues/FeaturesAvailable.cs
@@ -0,0 +1,17 @@
+﻿namespace ConfigValues
+{
+    public class FeaturesAvailable
+    {
+        public HomePage HomePage { get; set; }
+
+        public string LoggingEnabled { get; set; }
+    }
+
+    public class HomePage
+    {
+        public HomePage() { }
+
+        public bool ShowLoginPage { get; set; }
+
+    }
+}
