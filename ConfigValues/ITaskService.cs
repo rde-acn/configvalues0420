cd97ae1ef6a19488ba53a9c3bb2c342ef1890070
rde-acn <rde.acn.backpack@gmail.com>
2026-03-18 11:42:16 +0530

    Copy ConfigValues/ITaskService.cs from source repository

---
+++ ConfigValues/ITaskService.cs
@@ -0,0 +1,20 @@
+﻿using CoreWCF;
+
+namespace ConfigValues
+{
+    [ServiceContract]
+    public interface ITaskService
+    {
+        [OperationContract]
+        void TaskBoard();
+
+        [OperationContract]
+        string GetTask();
+
+        [OperationContract]
+        void SetTask(string Task);
+
+        [OperationContract]
+        bool ShowLoginPage();
+    }
+}
