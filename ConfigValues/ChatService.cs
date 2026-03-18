e77b45d7b599226316d3aefa6baebfafa4b12bc2
rde-acn <rde.acn.backpack@gmail.com>
2026-03-18 11:42:08 +0530

    Copy ConfigValues/ChatService.cs from source repository

---
+++ ConfigValues/ChatService.cs
@@ -0,0 +1,35 @@
+﻿using CoreWCF;
+using Microsoft.Extensions.Options;
+
+namespace ConfigValues
+{
+    
+    public class ChatService : IChatService
+    {
+        private ChatServiceConfiguration ChatServiceConfiguration;
+        public ChatService(IOptions<ChatServiceConfiguration> featuresConfiguration) 
+        {
+            ChatServiceConfiguration = featuresConfiguration.Value;
+        }
+        public string ReceiveMessage()
+        {
+            if (ChatServiceConfiguration.FeatureEnabled == true)
+            {
+                return "Message From Server";
+
+            } else
+            {
+                return "Chat Service NOT enabled !";
+            }
+        }
+
+        public void SendMessage()
+        {
+            if(ChatServiceConfiguration.FeatureEnabled == true)
+            {
+                Console.WriteLine("Sending message..");
+
+            }
+        }
+    }
+}
